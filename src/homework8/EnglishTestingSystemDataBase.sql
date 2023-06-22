60c5afd0d534fcf4ad71ab14ddd3569819469361
Ivan Haydarov <navi1435@gmail.com>
2023-06-22 23:30:14 +0700

    homework8 updated

--- src/homework8/EnglishTestingSystemDataBase.sql
+++ src/homework8/EnglishTestingSystemDataBase.sql
@@ -9,18 +9,44 @@ VALUES ('The doctor ___ in the office.'),
 	('She is late. She ___ missed the bus.');
 
 CREATE TABLE options
-(question_id INTEGER references questions (question_id),
-option_text TEXT[] NOT NULL UNIQUE, 
-correct_answer VARCHAR(100) NOT NULL
+(option_id SERIAL PRIMARY KEY,
+option_text VARCHAR(100) NOT NULL UNIQUE
 );
 
-INSERT INTO options(option_text, question_id, correct_answer)
-VALUES ('{am, is, are}', 1, 'is'),
-('{were, am, is, are}', 2, 'were'),
-('{should have, cannot have, could not have, was able to, must have}', 3, 'must have');
+INSERT INTO options(option_text)
+VALUES ('am'),
+('is'),
+('are'),
+('were'),
+('should have'),
+('can''t have'),
+('couldn''t have'),
+('was able to'),
+('must have');
 
+CREATE TABLE questions_options
+(question_id INTEGER references questions(question_id),
+option_id INTEGER references options(option_id),
+is_correct BOOLEAN NOT NULL,
+PRIMARY KEY(question_id, option_id)
+)
 
-SELECT q.*, o.*,
+INSERT INTO questions_options(question_id, option_id, is_correct)
+VALUES(1, 1, false),
+(1, 2, true),
+(1, 3, false),
+(2, 1, false),
+(2, 2, false),
+(2, 3, false),
+(2, 4, true),
+(3, 5, false),
+(3, 6, false),
+(3, 7, false),
+(3, 8, false),
+(3, 9, true);
+
+SELECT q.*, o.*, qo.is_correct
 FROM questions as q
-JOIN options as o ON q.question_id = o.question_id;
+JOIN questions_options as qo ON q.question_id =qo.question_id
+JOIN options as o ON qo.option_id = o.option_id
 
